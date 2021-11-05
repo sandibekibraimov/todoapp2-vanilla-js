627779b3457c38a88a93164c73dddb9f18e1297b
sandibekibraimov <sandibek@gmail.com>
2021-11-05 12:19:41 -0400

    adding todo and styles

--- app.js
+++ app.js
@@ -12,23 +12,24 @@ const addTodo = (e) => {
   todoDiv.classList.add('todo');
 
   const newTodo = document.createElement('li');
-  newTodo.innerText = 'test';
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo-item');
 
   todoDiv.appendChild(newTodo);
   // completed button
   const completedBtn = document.createElement('button');
-  completedBtn.innetHTML = '<i class="fas fa-check"></i>';
+  completedBtn.innerHTML = '<i class="far fa-check-square"></i>';
   completedBtn.classList.add('complete-btn');
   todoDiv.appendChild(completedBtn);
 
   // trash button
   const trashBtn = document.createElement('button');
-  trashBtn.innetHTML = '<i class="fas fa-trash"></i>';
+  trashBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
   trashBtn.classList.add('trash-btn');
   todoDiv.appendChild(trashBtn);
 
   todoList.appendChild(todoDiv);
+  todoInput.value = '';
 };
 
 // event listeners
